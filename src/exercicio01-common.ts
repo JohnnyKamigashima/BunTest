dcaf3bc7af89c72a01c939aea091db9df2d3aff2
Johnny Kamigashima <johnny@jhk.app>
2024-04-20 19:35:14 -0300

    Novas altgerações

--- src/exercicio01-common.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Se a entrada for uma string com separador virgula, sera ajustada para ponto
- * @param altura  altura
- * @returns numero da altura
- */
-export const corrigeNumero = (altura: any) => {
-    if (isNaN(altura)) return Number(altura.replace(',', '.'));
-    else return altura;
-}
--- src/exercicio02-funcoes.ts
+++
@@ -1,8 +0,0 @@
-export const estaDentroDoVetor = (item: string, vetor: string[]): boolean => {
-    // let resposta = false
-    // for (let elemento of vetor) if (elemento == item) resposta = true
-    // for (let indice in vetor) if (vetor[indice] == item) resposta = true
-    //     return resposta
-
-    return vetor.includes(item)
-}
